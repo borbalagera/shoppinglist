13308fe243bf3a47738946b581f4b3abcfacf1d7
Gera Borbála <37974097+borbalagera@users.noreply.github.com>
2020-02-20 17:50:34 +0100

    Add files via upload
    the html, css & js files

---
+++ shoppinlist/myScript.js
@@ -0,0 +1,44 @@
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+function addToList() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("inputVal").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You don't want to buy anything?");
+  } else {
+    document.getElementById("ulList").appendChild(li);
+  }
+  document.getElementById("inputVal").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
